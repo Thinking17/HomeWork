9bdf56e4fede5f4d4bd2744904523ed95d63814a
Wladimir Ongemach <ongemach.w@MacBook-Air-Wladimir.fritz.box>
2023-02-02 20:35:02 +0100

    Большее число из двух

--- Exersise/Program.cs
+++ Exersise/Program.cs
@@ -1,2 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Clear();
+
+Console.Write("Введите 1-ое число: ");
+int n = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите 2-ое число: ");
+int m = Convert.ToInt32(Console.ReadLine());
+
+// Пользователь вводит 2 числа необходимо найти максимум
+// n > m
+// n < m
+// n == m
+// = - оператор присвоения 
+// == - проверка на равенство 
+if (n > m )
+    Console.WriteLine(n);
+else if (n < m)
+    Console.WriteLine(m);
+else 
+    Console.WriteLine("=");
