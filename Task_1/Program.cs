aab9ba44df742628c4435e478dd747c0be3eafe0
Wladimir Ongemach <ongemach.w@MacBook-Air-Wladimir.fritz.box>
2023-02-04 12:00:08 +0100

    Task1

--- Task_1/Program.cs
+++ Task_1/Program.cs
@@ -1,4 +1,6 @@
-﻿Console.Clear();
+﻿// Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+
+Console.Clear();
 
 Console.Write("Введите 1-ое число: ");
 int a = Convert.ToInt32(Console.ReadLine());
