29e9599e424af8b38666cc74692a8633924ae5fa
Wladimir Ongemach <ongemach.w@MacBook-Air-Wladimir.fritz.box>
2023-02-04 11:30:38 +0100

    Программа готова

--- Exersise/Program.cs
+++ Exersise/Program.cs
@@ -3,3 +3,8 @@
 Console.Write("Введите 1-ое число: ");
 int a = Convert.ToInt32(Console.ReadLine());
 Console.Write("Введите 2-ое число: ");
+int b = Convert.ToInt32(Console.ReadLine());
+if (a > b )
+    Console.WriteLine($"{a}");
+else
+    Console.WriteLine($"{b}");
