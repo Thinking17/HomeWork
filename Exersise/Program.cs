6d4001504fb95353a938c77975ea53bed5a2567b
Wladimir Ongemach <ongemach.w@MacBook-Air-Wladimir.fritz.box>
2023-02-04 10:58:58 +0100

    new

--- Exersise/Program.cs
+++ Exersise/Program.cs
@@ -1,19 +1,5 @@
 ﻿Console.Clear();
 
 Console.Write("Введите 1-ое число: ");
-int n = Convert.ToInt32(Console.ReadLine());
+int a = Convert.ToInt32(Console.ReadLine());
 Console.Write("Введите 2-ое число: ");
-int m = Convert.ToInt32(Console.ReadLine());
-
-// Пользователь вводит 2 числа необходимо найти максимум
-// n > m
-// n < m
-// n == m
-// = - оператор присвоения 
-// == - проверка на равенство 
-if (n > m )
-    Console.WriteLine(n);
-else if (n < m)
-    Console.WriteLine(m);
-else 
-    Console.WriteLine("=");
